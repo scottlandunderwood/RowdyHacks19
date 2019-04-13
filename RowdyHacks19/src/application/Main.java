d07c72c857c37ac3e06690bb46ef54815fb1e019
zackkattack <zaccarson8715@gmail.com>
2019-04-13 17:52:40 -0500

    Created Main Controller for Main

--- RowdyHacks19/src/application/Main.java
+++ RowdyHacks19/src/application/Main.java
@@ -1,18 +1,25 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	public static Stage stage;
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage = primaryStage;
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("../View/MainPage.fxml"));
+			AnchorPane anchorPane = loader.load();
+
+			Scene scene = new Scene(anchorPane, 800, 800);
+
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
